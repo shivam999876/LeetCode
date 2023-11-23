82200c4f503bba3aa06d441d437c6cb351ef4711
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2023-11-23 12:18:26 +0530

    Time: 33 ms (30.22%) | Memory: 44.5 MB (38.19%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.java
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.java
@@ -1,41 +1,40 @@
-class Solution {
-    public Boolean check(int[] arr) {
+class Solution{
+
+    public boolean check(int[] arr) {
         int minElement = Integer.MAX_VALUE;
         int maxElement = Integer.MIN_VALUE;
-        Set<Integer> arrSet = new HashSet();
-        
+        Set<Integer> arrSet = new HashSet<>();
+
         for (int num : arr) {
             minElement = Math.min(minElement, num);
             maxElement = Math.max(maxElement, num);
             arrSet.add(num);
         }
-        
+
         if ((maxElement - minElement) % (arr.length - 1) != 0) {
             return false;
         }
-        
+
         int diff = (maxElement - minElement) / (arr.length - 1);
         int curr = minElement + diff;
-        
+
         while (curr < maxElement) {
             if (!arrSet.contains(curr)) {
                 return false;
             }
-            
+
             curr += diff;
         }
-        
+
         return true;
     }
-    
+
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        List<Boolean> ans = new ArrayList();
+        List<Boolean> ans = new ArrayList<>();
+
         for (int i = 0; i < l.length; i++) {
             int[] arr = new int[r[i] - l[i] + 1];
-            for (int j = 0; j < arr.length; j++) {
-                arr[j] = nums[l[i] + j];
-            }
-            
+            System.arraycopy(nums, l[i], arr, 0, arr.length);
             ans.add(check(arr));
         }
 
