0eaa53332c516f1b3a782ca96cc82f7a7444b3cf
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2023-12-07 22:44:51 +0530

    Time: 64 ms (6.71%) | Memory: 42.9 MB (22.56%) - LeetSync

--- 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.js
+++ 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.js
@@ -3,43 +3,61 @@
  * @return {string}
  */
 var tictactoe = function(moves) {
-    const n = 3;
-    const grid = Array.from({ length: n }, () => Array(n).fill(' '));
-
-    for (let i = 0; i < moves.length; i++) {
-        const [row, col] = moves[i];
-        const player = i % 2 === 0 ? 'A' : 'B';
-
-        grid[row][col] = player;
-
-        if (checkWinner(grid, row, col, player)) {
-            return player;
+    // Intializing the grid
+    let grid = [];
+    for(let i = 0; i < 3; i++){
+        grid[i] = [];
+        for(let j = 0; j< 3;j++ ){
+            grid[i][j] = 0;
         }
     }
 
-    return moves.length === n * n ? 'Draw' : 'Pending';
-};
-
-const checkWinner = (grid, row, col, player) => {
-    // Check row
-    if (grid[row].every(cell => cell === player)) {
-        return true;
+    //Reading the moves of player A
+    for (let i = 0; i<moves.length; i+=2){
+        let [row, col] = moves[i];
+        grid[row][col] = 1;
     }
 
-    // Check column
-    if (grid.every(row => row[col] === player)) {
-        return true;
+    //Reading the moves of player B
+    for (let i = 1; i<moves.length; i+=2){
+        let [row, col] = moves[i];
+        grid[row][col] = -1;
+    }
+    let rowsResult = []
+
+    //Checking each row and column for a winner 
+    for (let i = 0; i<3; i++){
+        let rowValue = 0;
+        let columnValue = 0;
+
+        for (let j = 0; j<3; j++){ 
+           rowValue+= grid[i][j];
+           columnValue += grid[j][i];
+        }
+
+        if (rowValue === 3 || columnValue === 3) return "A";
+        else if (rowValue === -3 || columnValue === -3) return "B";
+        else rowsResult.push(rowValue);
     }
 
-    // Check diagonal (if applicable)
-    if (row === col && grid.every((row, i) => row[i] === player)) {
-        return true;
+    //Checking diganols for a winner
+    let firstDiagonal = 0; 
+    let secondDiagonal = 0;
+    for(let i=0; i<3; i++){
+        firstDiagonal += grid[i][i];
+        secondDiagonal += grid[i][2-i];
     }
+    console.log(firstDiagonal)
+    console.log(secondDiagonal)
+    if (firstDiagonal === 3 || secondDiagonal === 3) return "A";
+    if (firstDiagonal === -3 || secondDiagonal === -3) return "B";
 
-    // Check anti-diagonal (if applicable)
-    if (row + col === grid.length - 1 && grid.every((row, i) => row[grid.length - 1 - i] === player)) {
-        return true;
+    // console.log(grid)
+    // Checking for DRAW or PENDING 
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j< 3;j++ ){
+            if (grid[i][j] === 0) return "Pending";
+        }
     }
-
-    return false;
+    return "Draw";
 };
