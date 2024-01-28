bee4318ad396122a292c05d9d537e19ee6e0472c
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2024-01-28 21:07:12 +0530

    Time: 7 ms (45.17%) | Memory: 42.3 MB (81.72%) - LeetSync

---
+++ 1316-fizz-buzz-multithreaded/fizz-buzz-multithreaded.java
@@ -0,0 +1,71 @@
+class FizzBuzz {
+    // taken help
+    
+    private int n;
+    private int i;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+        this.i = 1;
+    }
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        synchronized (this) {
+            while (i<=n) {
+                if (i%3==0 && i%5!=0) {
+                    printFizz.run();
+                    i+=1;
+                    notifyAll();            
+                } else {
+                    wait();
+                }
+            }
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        synchronized (this) {
+            while (i<=n) {
+                if (i%3!=0 && i%5==0) {
+                    printBuzz.run();
+                    i+=1;
+                    notifyAll();            
+                } else {
+                    wait();
+                }
+            }
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        synchronized (this) {
+            while (i<=n) {
+                if (i%15==0) {
+                    printFizzBuzz.run();
+                    i+=1;
+                    notifyAll();            
+                } else {
+                    wait();
+                }
+            }
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        synchronized (this) {
+            while (i<=n) {
+                if (i%3!=0 && i%5!=0) {
+                    printNumber.accept(i);
+                    i+=1;
+                    notifyAll();            
+                } else {
+                    wait();
+                }
+            }
+        }
+    }
+}
