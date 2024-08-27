3c58d90563a938ef882c41c395ad6fc0bf8e1be2
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2024-08-27 21:40:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (6.85%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,23 @@
 class Solution {
+    public int fibon(int n, ArrayList<Integer> dp) {
+        // Base Case
+        if (n <= 1)
+            return n;
+        
+        // Check if value is already calculated
+        if (dp.get(n) != -1)
+            return dp.get(n);
+        
+        // Memoization
+        dp.set(n, fibon(n - 1, dp) + fibon(n - 2, dp));
+        return dp.get(n);
+    }
+
     public int fib(int n) {
-        //Base Case
-        if(n <= 1)
-        return n;
-        return fib(n-1) + fib(n-2);
+        ArrayList<Integer> dp = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            dp.add(-1);
+        }
+        return fibon(n, dp);
     }
 }
