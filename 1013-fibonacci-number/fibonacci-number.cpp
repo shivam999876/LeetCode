44a5fa9a91bfce4e682553ccab2d7e08a0603e87
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2024-08-27 22:17:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (10.36%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,12 +3,20 @@ public:
 int fibon(int n, vector<int>&dp){
     //Base Case
     if(n<=1)
-        return n;
-        return fibon(n-1,dp)+fibon(n-2,dp);
+    return 0;
+    //Already calculated result (fibon number)
+    if(dp[n]!=-1)
+    return dp[n];
+    return dp[n] = fibon(n-1,dp)+fibon(n-2,dp);
 }
-    //Memoization
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return fibon(n,dp);
+        if(n<=1)
+        return n;
+    vector<int>dp(n+1,-1);
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<=n;i++)
+    dp[i] = dp[i-1]+dp[i-2];
+    return dp[n];
     }
 };
