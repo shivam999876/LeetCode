1a044b6da27367916c2c2e96889d5c3b8b8e71e8
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2024-10-28 22:22:16 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (43.74%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,16 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-           // Total sum of first N numbers
-        int n = nums.length + 1;
-        int totalSum = n * (n - 1) / 2;
-
-        // Calculate sum of all array elements
-        int arraySum = 0;
-        for (int num : nums) {
-            arraySum += num;
+        int n_xor = nums.length;
+        for(int i = 0; i < nums.length; i++){
+            n_xor = n_xor ^ i;
+            n_xor = n_xor ^ nums[i];
         }
-
-        // The missing number is the difference between the total sum and the array sum
-        return totalSum - arraySum;
+        return n_xor;
+        
     }
 }
