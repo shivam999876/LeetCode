5daf9d777e3cec79fb9acfa35017f3b46c64e109
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2024-11-24 16:18:11 +0530

    Time: 27 ms (99.65%) | Memory: 56.3 MB (83.67%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -1,22 +1,46 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-         Arrays.sort(nums);
-        int i = 0;
-        int j = 0;
-        int sum = 0;
-        int maxLength = 0;
-
-        for (i = 0; i < nums.length; i++) {
-            sum += nums[i];
-
-            while ((i - j + 1) * nums[i] - sum > k) {
-                sum -= nums[j];
-                j++;
+        Arrays.sort(nums);
+// so that we can easily see the freuency of elements
+        int right = 0;
+// for right index
+        int maxCount = 0;
+        int left = -1;
+// 
+        int totalOp = 0;
+        int c = 0;
+        while(right<nums.length){
+            int start = right;
+            while(right<nums.length && nums[right] == nums[start]){
+                right++;
             }
-
-            maxLength = Math.max(maxLength, i - j + 1);
+            int count = right-start;
+            if(left==-1 && c<2){
+                c++;
+                left = start-1;
+                totalOp = 0;
+                while(left>=0){
+                    if(totalOp+nums[start]-nums[left] <= k){
+                        totalOp+=(nums[start]-nums[left]);
+                        count++;
+                    }
+                    else{
+                        break;
+                    }
+                    left--;
+                }
+            }
+            else{
+                totalOp+=((nums[start]-nums[start-1])*(start-left-1));
+                count += start-left-1;
+                while(totalOp>k && left<start){
+                    totalOp-=  nums[start]-nums[left+1];
+                    left++;
+                    count--;
+                }
+            }
+            maxCount = Math.max(maxCount,count);
         }
-
-        return maxLength;
+        return maxCount;
     }
 }
