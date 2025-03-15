4ec866771e23d403dbce000367426ed5b5edcd92
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-03-15 13:38:37 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (68.89%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,40 +1,37 @@
+class DisjointSet {
+    vector<int> parent, depth;
+public:
+    DisjointSet(int n) : parent(n), depth(n) {
+        depth.assign(n, 1);
+        iota(parent.begin(), parent.end(), 0);
+    }
+
+    int getRoot(int x) {
+        if (x == parent[x])
+            return x;
+        return parent[x] = getRoot(parent[x]);
+    }
+
+    bool merge(int x, int y) {
+        int rootX = getRoot(x), rootY = getRoot(y);
+        if (rootX == rootY)
+            return false;
+        if (depth[rootX] > depth[rootY])
+            swap(rootX, rootY);
+        parent[rootX] = rootY;
+        if (depth[rootX] == depth[rootY])
+            depth[rootY]++;
+        return true;
+    }
+};
+
 class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        unordered_map<int, vector<int>> graph;
-
-        auto isConnected = [&](int u, int v) {
-            unordered_set<int> visited;
-            stack<int> stack;
-            stack.push(u);
-
-            while (!stack.empty()) {
-                int node = stack.top();
-                stack.pop();
-
-                if (visited.count(node)) continue;
-                visited.insert(node);
-
-                if (node == v) return true;
-
-                for (int neighbor : graph[node]) {
-                    stack.push(neighbor);
-                }
-            }
-            return false;
-        };
-
-        for (const auto& edge : edges) {
-            int u = edge[0], v = edge[1];
-
-            if (graph.count(u) && graph.count(v) && isConnected(u, v)) {
-                return edge;
-            }
-
-            graph[u].push_back(v);
-            graph[v].push_back(u);
-        }
-
+        int n = edges.size();
+        DisjointSet ds(n + 1);
+        for (auto& edge : edges)
+            if (!ds.merge(edge[0], edge[1])) return edge;
         return {};
     }
 };
