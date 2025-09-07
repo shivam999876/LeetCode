11442745fc07b2dc2785e7dfd66674d720ea9e20
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-09-07 14:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 55.9 MB (34.48%) - LeetSync

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+const sumZero = n => {
+    const res = new Array(n);
+    res[0] = n * (1 - n) / 2;
+    for(let i = 1; i < n; ++i)
+          res[i] = i;
+    return res;
+};
