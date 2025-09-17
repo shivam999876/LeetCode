80210d54eaef4696f46b78f087f82952733cbc7e
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-09-17 08:48:44 +0530

    Time: 205 ms (50.91%) | Memory: 73.9 MB (61.21%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.java
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.java
@@ -1,51 +1,44 @@
-class FoodRatings {
-    // Map food with its rating.
-    private Map<String, Integer> foodRatingMap = new HashMap<>();
-    // Map food with cuisine it belongs to.
-    private Map<String, String> foodCuisineMap = new HashMap<>();
+import java.util.*;
 
-    // Store all food of cuisine in set (to sort them on ratings/name)
-    // Set element -> Pair: (-1 * foodRating, foodName)
-    private Map<String, TreeSet<Pair<Integer, String>>> cuisineFoodMap = new HashMap<>();
+class Food {
+    String name;
+    String cuisine;
+    int rating;
+    Food(String n, String c, int r) {
+        name = n;
+        cuisine = c;
+        rating = r;
+    }
+}
+
+class FoodRatings {
+    private Map<String, Food> foodToFood;
+    private Map<String, TreeSet<Food>> cuisineToFoods;
 
     public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
-        for (int i = 0; i < foods.length; ++i) {
-            // Store 'rating' and 'cuisine' of current 'food' in 'foodRatingMap' and 'foodCuisineMap' maps.
-            foodRatingMap.put(foods[i], ratings[i]);
-            foodCuisineMap.put(foods[i], cuisines[i]);
-
-            // Insert the '(-1 * rating, name)' element in the current cuisine's set.
-            cuisineFoodMap
-                .computeIfAbsent(cuisines[i], k -> new TreeSet<>((a, b) -> {
-                    int compareByRating = Integer.compare(a.getKey(), b.getKey());
-                    if (compareByRating == 0) {
-                        // If ratings are equal, compare by food name (in ascending order).
-                        return a.getValue().compareTo(b.getValue());
-                    }
-                    return compareByRating;
+        foodToFood = new HashMap<>();
+        cuisineToFoods = new HashMap<>();
+        for (int i = 0; i < foods.length; i++) {
+            Food f = new Food(foods[i], cuisines[i], ratings[i]);
+            foodToFood.put(foods[i], f);
+            cuisineToFoods
+                .computeIfAbsent(cuisines[i], k -> new TreeSet<>( (a, b) -> {
+                    if (a.rating != b.rating) return Integer.compare(b.rating, a.rating);
+                    return a.name.compareTo(b.name);
                 }))
-                .add(new Pair(-ratings[i], foods[i]));
+                .add(f);
         }
     }
-
+    
     public void changeRating(String food, int newRating) {
-        // Fetch cuisine name for food.
-        String cuisineName = foodCuisineMap.get(food);
-
-        // Find and delete the element from the respective cuisine's set.
-        TreeSet<Pair<Integer, String>> cuisineSet = cuisineFoodMap.get(cuisineName);
-        Pair<Integer, String> oldElement = new Pair<>(-foodRatingMap.get(food), food);
-        cuisineSet.remove(oldElement);
-
-        // Update food's rating in 'foodRating' map.
-        foodRatingMap.put(food, newRating);
-        // Insert the '(-1 * new rating, name)' element in the respective cuisine's set.
-        cuisineSet.add(new Pair<>(-newRating, food));
+        Food f = foodToFood.get(food);
+        TreeSet<Food> set = cuisineToFoods.get(f.cuisine);
+        set.remove(f);
+        f.rating = newRating;
+        set.add(f);
     }
-
+    
     public String highestRated(String cuisine) {
-        Pair<Integer, String> highestRated = cuisineFoodMap.get(cuisine).first();
-        // Return name of the highest rated 'food' of 'cuisine'.
-        return highestRated.getValue();
+        return cuisineToFoods.get(cuisine).first().name;
     }
 }
