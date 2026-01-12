e0ea09aef0d11f617d10ce49a48b65e55c6f5948
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-01-12 21:06:53 +0530

    Time: 1 ms (95.28%) | Memory: 45 MB (30.73%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -1,19 +1,25 @@
 class Solution {
-    
-    public int toTime(int[] from, int[] to) {
-        int xDiff = Math.abs(from[0] - to[0]);
-        int yDiff = Math.abs(from[1] - to[1]);
-        
-        return Math.max(xDiff, yDiff);
-    }
-    
     public int minTimeToVisitAllPoints(int[][] points) {
-        int time = 0;
-    
-        for (int i = 1; i < points.length; i++) {
-            time += toTime(points[i - 1], points[i]);
+        int n = points.length;
+        
+        int steps = 0;
+        
+        for (int i = 0; i < n - 1; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            
+            int x2 = points[i + 1][0];
+            int y2 = points[i + 1][1]; 
+            
+            int dx = Math.abs(x2 - x1);
+            int dy = Math.abs(y2 - y1);
+            
+            int diagonal = Math.min(dx, dy);
+            int remain = Math.abs(dx - dy);
+            
+            steps += diagonal + remain;
         }
         
-        return time;
+        return steps;
     }
 }
