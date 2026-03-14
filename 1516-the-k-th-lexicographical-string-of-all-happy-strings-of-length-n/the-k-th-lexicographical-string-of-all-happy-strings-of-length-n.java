e5cccda640a761ed4bd5f5c2dd1f183f53b2aafa
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-03-14 13:13:38 +0530

    Time: 2 ms (76.72%) | Memory: 43.1 MB (87.15%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void solve(int n, StringBuilder curr, int[] count, int k, String[] result) {
+        if (curr.length() == n) {
+            count[0]++;
+            if (count[0] == k) {
+                result[0] = curr.toString();
+            }
+            return;
+        }
+        
+        for (char ch = 'a'; ch <= 'c'; ch++) {
+            if (curr.length() > 0 && curr.charAt(curr.length() - 1) == ch)
+                continue;
+            curr.append(ch);
+            solve(n, curr, count, k, result);
+            if (!result[0].isEmpty()) return;
+            curr.deleteCharAt(curr.length() - 1);
+        }
+    }
+
+    public String getHappyString(int n, int k) {
+        StringBuilder curr = new StringBuilder();
+        String[] result = {""}; 
+        int[] count = {0};  
+        
+        solve(n, curr, count, k, result);
+
+        return result[0];
+    }
+}
