9dfb178ab220e9b266edc3bb426a75ed1808f73e
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-06-12 12:49:17 +0530

    Time: 3 ms (99.78%) | Memory: 94.6 MB (43.48%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,39 +1,42 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
+        if(head == null || head.next == null){
+            return true ;
+
         }
-        ListNode tail = reverse(slow);
-        while(tail!=null){
-            if(head.val != tail.val){
+        ListNode slow = head;
+        ListNode fast  = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode prev = null;
+        while (slow != null){
+            ListNode nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+        ListNode first = head;
+        ListNode  second = prev;
+        while (second != null){
+            if( first.val != second.val){
                 return false;
             }
-            head= head.next;
-            tail= tail.next ; 
+            first = first.next;
+            second = second.next;
         }
-        return true;
-    }
-    public static ListNode reverse(ListNode node) {
-        ListNode prev = null;
-        while (node != null) {
-            ListNode temp = node.next;
-            node.next = prev;
-            prev = node;
-            node = temp;
-        }
-     return prev;
+       return true;
     }
 }
