08732d855ad4a45218854dfb804cdd9850a03792
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-07-01 17:16:39 +0530

    Time: 29 ms (28.74%) | Memory: 47.7 MB (27.65%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minInsertions(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+        int n = s.length();
+        int dp[][] = new int[n + 1][n + 1];
+        for(int i = 0; i <= n; i++) dp[i][0] = 0;
+        for(int j = 0; j <= n; j++) dp[0][j] = 0;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(s.charAt(i - 1) == rev.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return n - dp[n][n];
+    }
+}
